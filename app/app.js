e1650554443a25a0b11c83df0f037bbae463c4cf
Nick Nisi <nick@nisi.org>
2013-01-03 12:06:12 -0600

    Adding all examples to '/app'

--- app/app.js
+++ app/app.js
@@ -1,35 +1,45 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require("express"),
+    fs = require("fs"),
+    lessMiddleware = require("less-middleware");
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+var middleware = function (req, res, next) {
+    // do something important
+    // or don't.
+    // i'm a comment, not a cop
+    fs.readFile(__dirname + '/name.txt', function (err, data) {
+        req.name = data;
+        next();
+    });
+};
+
+app.configure(function () {
+    app.set('view engine', 'hbs');
+    app.use(lessMiddleware({
+        src: __dirname + '/public'
+    }));
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.get('/foo', middleware, function (req, res) {
+    res.send("Hello,  " + req.name + " from the route!");
 });
+
+app.get('/greeting/:name/:location', function (req, res) {
+    res.render("user", {
+        name: req.params.name,
+        location: req.params.location
+    });
+});
+
+app.use(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, Middleware");
+});
+
+app.listen(3000);
